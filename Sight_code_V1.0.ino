f813e5dfa5a588e414980c835c426eaf82a9f310
jules costille <julesgcc8804@gmail.com>
2021-03-28 17:18:08 +0200

    optimisatin du code sightV1.0 et ajout des effets avec keypad et exemplt tflip flop et ajout d'un fichier texte pour les problèmes en cours

--- Sight_code_V1.0.ino
+++ Sight_code_V1.0.ino
@@ -17,6 +17,9 @@
 
 
 
+
+
+/*-------------------------------------------------NEOPIXEL---------------------------------------------------------*/
 #define BacklightPin 16 //Pin arrière
 #define FrontlightPin 4 //Pin avant
 #define IndicateurPin 2 // Indicateur télécommande
@@ -45,6 +48,10 @@ uint32_t white = Frontlight.Color(255 , 255 , 255 , 0);    //Blanc avec Led blan
 uint32_t orange strip.Color(150 , 150 , 0 , 0);   //Orange*/
 uint32_t otaneo = Indicateur.Color(0,255,150);
 
+/*-----------------------------------------------------------NEOPIXEL--------------------------------------------------------------*/
+
+
+
 
 //IHM
 int PowerIndicateurLed = 13 ; //Pin LED indicateur ON/FF
@@ -65,8 +72,12 @@ int TempBatPin2 = 35 ;
 int UbatPin = 32 ; //Tension batterie Pin
 
 int i = 0; //variable pour effet lumière
+int i_front = 0 ;
+int i_back = 0 ;
 
 
+
+/*------------------------------------KEYPAD---------------------------------------------------------------*/
 const byte ROWS = 3 ;
 const byte COLS = 2 ;
 
@@ -81,18 +92,16 @@ byte colPins[COLS]= { 19 , 21 };
 
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys  ),rowPins,colPins,ROWS,COLS);
 
+/*--------------------------------------------------KEYPAD-----------------------------------------------------*/
+
 
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Booting");
-  setupOTA("Sight_ESP32_V1.0_1", "esp32", "Crousti8804"); //Setup pour  pour librairie OTA
-//              Nom OTA       Nom réseau   Mot de passe réseau*
-
-
-
+  setupOTA("Sight_ESP32_V1.0_1", "esp32", "Crousti8804"); //Setup pour  pour librairie OTA (nom réseau ota, nom réseau auquel se connecter, mot de passe réseau)
+  // attention ne pas décommenter problème : la carte passe en mode sleep automatique et ne se réveille pas normalement
   //esp_sleep_enable_ext0_wakeup(GPIO_NUM_12,HIGH);//Active le réveille externe par le bouton power
-
 
   Backlight.begin();
   Frontlight.begin();
@@ -102,11 +111,11 @@ void setup() {
   Indicateur.setBrightness(brightnessIndicateur);
   Backlight.fill(white,0,30);
   Frontlight.fill(white,0,30);
+  Indicateur.fill(white,0,3);
   Backlight.show();
   Frontlight.show();
   Indicateur.show();
   delay(2000);
-
 
   pinMode(PowerIndicateurLed,OUTPUT);
   digitalWrite(PowerIndicateurLed,HIGH);
@@ -114,28 +123,15 @@ void setup() {
   //Bouton Power ON/OFF
   pinMode(PowerButton,INPUT);
 
-
-  
-
-
-
-
-
-
 }
 
 void loop() {
-/*  #ifdef defined(ESP32_RTOS) && defined(ESP32)
-#else // If you do not use FreeRTOS, you have to regulary call the handle method.
-  ArduinoOTA.handle();
-#endif*/
 
 #ifdef defined(ESP32_RTOS) && defined(ESP32)
 #else // If you do not use FreeRTOS, you have to regulary call the handle method.
   ArduinoOTA.handle();
 #endif
   TelnetStream.println("Loop");
-
 
 
   char customKey = customKeypad.getKey();
@@ -181,12 +177,7 @@ if(PowerButtonState == HIGH){
   Serial.print("PowerButtonState :");
   Serial.println(PowerButtonState);
 
-
-
-
-
-
-
+/*--------------------------------------------CAPTEUR DE TEMPERATURE---------------------------------------*/
   //Température UP
   /*Serial.println("-------------");
   int TempUPRead = analogRead(TempUPPin);//Récupérer tension de sortie du capteur
@@ -203,11 +194,6 @@ if(PowerButtonState == HIGH){
   Serial.println("-------------");
 */
 
-
-
-
-
-
   //Température down
   /*TempDownval = analogRead(TempDown);
   TempDown = 5*TempDownval/1024;
@@ -215,8 +201,6 @@ if(PowerButtonState == HIGH){
   Serial.println(TempDownval);
   Serial.print("TEMPDOWN °C =");
   Serial.println(TempDown);*/
-
-
 
 
   //tempétature batterie
@@ -240,8 +224,7 @@ if(PowerButtonState == HIGH){
   Serial.print("tempCBat :");
   Serial.println(tempCBat);
   Serial.println("-------------");
-
-
+/*------------------------------------CAPTEUR DE TEMPERATURE------------------------------------------------*/
 
 
   //Tension batterie
@@ -255,10 +238,7 @@ if(PowerButtonState == HIGH){
   Serial.print("Ubat =");
   Serial.println(Ubat);
 
-
-
-
-  delay(3000);
+  delay(3000);//delay pour print serial
 
 
 
@@ -291,11 +271,12 @@ if(PowerButtonState == HIGH){
 
 
   if(customKeypad=2){
-
+    
   }
 
 
   if(customKeypad=3){
+
 
   }
 
@@ -304,16 +285,90 @@ if(PowerButtonState == HIGH){
 
   }
 
+*/
 
-
-  if(customKeypad=5){
-
+  if(customKeypad=5)
+  {
+    while(i_front<30 and i_back<15)
+    {
+      i_front++;
+      i_back++;
+      Frontlight.setPixelColor(i_front,255,150,0);
+      Backlight.setPixelColor(i_back,255,150,0);
+      Frontlight.show();
+      Backlight.show();
+      delay(50);
+    }  
   }
 
 
 
   if(customKeypad=6){
+    i_front = 30 ;
+    i_back = 30 ;
+    while(i_front<0 and i_back<15)
+    {
+      i_front--;
+      i_back--;
+      Frontlight.setPixelColor(i_front,255,150,0);
+      Backlight.setPixelColor(i_back,255,150,0);
+      Frontlight.show();
+      Backlight.show();
+      delay(50);
+
+    }
 
   }
-*/
+
+}
+
+
+/* switch
+ * 
+ * Each time the input pin goes from LOW to HIGH (e.g. because of a push-button
+ * press), the output pin is toggled from LOW to HIGH or HIGH to LOW.  There's
+ * a minimum delay between toggles to debounce the circuit (i.e. to ignore
+ * noise).  
+ *
+ * David A. Mellis
+ * 21 November 2006
+ */
+
+int inPin = 2;         // the number of the input pin
+int outPin = 13;       // the number of the output pin
+
+int state = HIGH;      // the current state of the output pin
+int reading;           // the current reading from the input pin
+int previous = LOW;    // the previous reading from the input pin
+
+// the follow variables are long's because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long time = 0;         // the last time the output pin was toggled
+long debounce = 200;   // the debounce time, increase if the output flickers
+
+void setup()
+{
+  pinMode(inPin, INPUT);
+  pinMode(outPin, OUTPUT);
+}
+
+void loop()
+{
+  reading = digitalRead(inPin);
+
+  // if the input just went from LOW and HIGH and we've waited long enough
+  // to ignore any noise on the circuit, toggle the output pin and remember
+  // the time
+  if (reading == HIGH && previous == LOW && millis() - time > debounce) {
+    if (state == HIGH)
+      state = LOW;
+    else
+      state = HIGH;
+
+    time = millis();    
+  }
+
+  digitalWrite(outPin, state);
+
+  previous = reading;
 }
